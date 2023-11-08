d4327ae304838167ae41ccf525f4580b1efa1996
Qiming <LuQiMing@aactechnologies.com>
2023-11-08 17:02:47 +0800

    初始修改：升级gradle，使用androidX

--- app/src/main/java/com/amitshekhar/tflite/MainActivity.java
+++ app/src/main/java/com/amitshekhar/tflite/MainActivity.java
@@ -2,7 +2,7 @@ package com.amitshekhar.tflite;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
